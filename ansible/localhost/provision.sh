724918581540f59df80d6cff4a701b5bb72611c1
Robert G. Johnson Jr <rjohnson@oakensoul.com>
2015-07-05 18:37:43 -0400

    Adding Vagrantfile, local provisioner, and Ansible to install PHP 5.6 on ubuntu/trusty64

---
+++ ansible/localhost/provision.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    echo "Installing Ansible:"
+    export DEBIAN_FRONTEND=noninteractive
+
+    echo "  1/5. Update apt"
+    apt-get update -qq
+
+    echo "  2/5. Install python-software-properties"
+    apt-get install -qq python-software-properties &> /dev/null || exit 1
+
+    echo "  3/5. Add Ansible PPA"
+    apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
+
+    echo "  4/5. Update apt to grab new PPA info for Ansible"
+    apt-get update -qq
+
+    echo "  5/5. Install Ansible"
+    apt-get install -qq ansible &> /dev/null || exit 1
+
+    echo "Ansible installed"
+fi
+
+cd /vagrant/ansible
+ansible-playbook localhost/vagrant.yml --inventory-file=inventory/vagrant.ini --connection=local
