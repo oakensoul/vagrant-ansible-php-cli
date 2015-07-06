7dea98174314247ffb5fc4a5aaff7036d2aaf71f
Robert G. Johnson Jr <rjohnson@oakensoul.com>
2015-07-05 23:46:15 -0400

    Updating/Switching to use Ansible Citadel. Also adding multi-box

--- ansible/localhost/provision.sh
+++ ansible/localhost/provision.sh
@@ -5,25 +5,7 @@ then
     echo "Configuring the Server:"
     export DEBIAN_FRONTEND=noninteractive
 
-    echo "  1/7. Update apt"
-    apt-get update -qq &> /dev/null || exit 1
-
-    echo "  2/7. Install python-software-properties python-apt python-pycurl"
-    apt-get install -qq python-software-properties python-apt python-pycurl &> /dev/null || exit 1
-
-    echo "  3/7. Add Ansible PPA"
-    apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
-
-    echo "  4/7. Update apt to grab new PPA info for Ansible"
-    apt-get update -qq &> /dev/null || exit 1
-
-    echo "  5/7. Install Ansible"
-    apt-get install -qq ansible &> /dev/null || exit 1
-
-    echo "  6/7. Remove auto-installed packages that are no longer required"
-    apt-get -y autoremove &> /dev/null || exit 1
-
-    echo "  7/7. Upgrading all packages"
+    echo "  1/1. Upgrading all packages"
     apt-get -y dist-upgrade &> /dev/null || exit 1
 
 fi
