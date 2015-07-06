0c3ac38894b37e72d79ae584290379cc4176245d
Robert G. Johnson Jr <rjohnson@oakensoul.com>
2015-07-05 21:03:23 -0400

    Updating Comments

--- ansible/localhost/provision.sh
+++ ansible/localhost/provision.sh
@@ -2,28 +2,30 @@
 
 if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    echo "Installing Ansible:"
+    echo "Configuring the Server:"
     export DEBIAN_FRONTEND=noninteractive
 
-    echo "  1/5. Update apt"
-    apt-get update -qq
+    echo "  1/7. Update apt"
+    apt-get update -qq &> /dev/null || exit 1
 
-    echo "  2/5. Install python-software-properties python-apt python-pycurl"
+    echo "  2/7. Install python-software-properties python-apt python-pycurl"
     apt-get install -qq python-software-properties python-apt python-pycurl &> /dev/null || exit 1
 
-    echo "  3/5. Add Ansible PPA"
+    echo "  3/7. Add Ansible PPA"
     apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
 
-    echo "  4/5. Update apt to grab new PPA info for Ansible"
-    apt-get update -qq
+    echo "  4/7. Update apt to grab new PPA info for Ansible"
+    apt-get update -qq &> /dev/null || exit 1
 
-    echo "  5/5. Install Ansible"
+    echo "  5/7. Install Ansible"
     apt-get install -qq ansible &> /dev/null || exit 1
 
-    echo "Ansible installed"
+    echo "  6/7. Remove auto-installed packages that are no longer required"
+    apt-get -y autoremove &> /dev/null || exit 1
 
-    echo "Upgrading all packages"
-    apt-get dist-upgrade
+    echo "  7/7. Upgrading all packages"
+    apt-get -y dist-upgrade &> /dev/null || exit 1
+
 fi
 
 cd /vagrant/ansible
